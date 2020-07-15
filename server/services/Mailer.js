ceba90954fc2f42f71db46eafa66effcde9a31e1
Donald Fox <fox_donald@yahoo.com>
2020-07-14 23:00:11 -0400

    Create email template and format body output

---
+++ server/services/Mailer.js
@@ -0,0 +1,17 @@
+const sendgrid = require('sendgrid')
+const helper = sendgrid.mail
+const keys = require('../config/keys')
+const email = require('../config/dev')
+
+class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super()
+
+        this.from_email = new helper.Email(email.senderEmail)
+        this.subject = subject
+        this.body = new helper.Content('text/html', content)
+        this.recipients = this.formatAddresses(recipients)
+    }
+}
+
+module.exports = Mailer
