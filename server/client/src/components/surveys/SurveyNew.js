fe02a1672779378b6123f8c466b96a0acad83f78
Donald Fox <fox_donald@yahoo.com>
2020-07-20 21:00:39 -0400

    Resolve regex warning (emailregex.com), add SurveyFormReview component, and state to SurveyNew

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import SurveyForm from './SurveyForm'
+import SurveyFormReview from './SurveyFormReview'
 
 class SurveyNew extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { new: true }
+    }
+
     render() {
         return (
             <div>
