48a5f30b22bb85ac2c7cc200eb862af102021dac
Donald Fox <fox_donald@yahoo.com>
2020-07-26 23:15:12 -0400

    Update query and update routes

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,14 +10,14 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 const Survey = mongoose.model('surveys')
 
 module.exports = (app) => {
-    app.get('/api/surveys/thanks', (seq, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (seq, res) => {
         res.send('Thanks for voting!')
     })
 
     app.post('/api/surveys/webhooks', (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice')
 
-        const events = _.chain(req.body)
+        _.chain(req.body)
             .map(({ email, url }) => {
                 const match = p.test(new URL(url).pathname)
 
@@ -31,9 +31,25 @@ module.exports = (app) => {
             })
             .compact()
             .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: {
+                                email: email,
+                                responded: false
+                            }
+                        }
+                    },
+                    {
+                        $inc: { [choice]: 1 },
+                        $set: { 'recipients.$.responded': true },
+                        lastResponded: new Date()
+                    }
+                ).exec()
+            })
             .value()
-
-        console.log(events)
 
         res.send({})
     })
